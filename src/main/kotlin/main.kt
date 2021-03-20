49694ca391fdd8aa1e76394671b9e8d66892c490
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-20 14:22:59 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,17 @@
+enum class Dlugosc {
+    MILIMETRY, CENTYMETRY, METRY, KILOMETRY
+}
+
 fun main() {
+    var dl0: Dlugosc = Dlugosc.MILIMETRY
+    var dl1: Dlugosc = Dlugosc.MILIMETRY
+
+    if(dl0==Dlugosc.METRY)
+        println("dl0 to dlugosc w metrach")
+
+    if(dl0==Dlugosc.MILIMETRY)
+        println("dl0 to dlugosc w milimetrach")
+
+
+
 }
